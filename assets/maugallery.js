3d43290a0bea9afd8f58968cd6039186bbfd82cd
Leo-Brd <leobordet.pro@gmail.com>
2024-12-13 15:28:51 +0100

    Fix filters buttons tab access

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -223,7 +223,7 @@
         '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+                <span class="nav-link" tabindex="0" aria-label="Filtrer la catégorie ${value}" data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
 
